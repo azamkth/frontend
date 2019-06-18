3e8ac32d4e59306755ac898b47d92e976bfd2ac9
Azam <azam3cs@gmail.com>
2019-06-18 13:40:56 +0200

    test

---
+++ VueForm/test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Show template</p>
+    </div>
+</template>
+<script>
+export default {
+    name:"hello world",
+    data (){
+        msg : 'Welcome to your Vue.js APP'
+    }    
+}
+</script>
+
+        
+
+       
