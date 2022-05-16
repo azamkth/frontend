cc89cc3771366c9a9ecfaaa3c138dcfef14a91bf
Azam <azam3cs@gmail.com>
2022-05-16 15:50:44 +0200

    added bottom tab

--- react-native-project/ReactNativeProject/components/parents/Parents.js
+++ react-native-project/ReactNativeProject/components/parents/Parents.js
@@ -5,10 +5,36 @@ import {
     Text
 } from 'react-native';
 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Test from './Test';
+
+
+const Tab = createBottomTabNavigator();
+
 export default function Parents(props) {
     return (
-        <View>
-            <Text>Hello i am Parents</Text>
-        </View>    
+        <Tab.Navigator
+            screenOptions={{
+                    activeTintColor: '#000',
+                    inactiveTintColor: 'gray',
+                    style: {
+                        backgroundColor: '#F5F5F5',
+                    },
+                }}
+            >
+            <Tab.Screen
+                name="Uppdrag"
+                component={Test}
+            />
+            <Tab.Screen
+                name="Köpta"
+                component={Test}
+            />
+            <Tab.Screen
+                name="Vunna"
+                component={Test}
+            />
+        </Tab.Navigator>
     );
 }
---
+++ react-native-project/ReactNativeProject/components/parents/Test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { 
+    View,
+    StyleSheet,
+    Text
+} from 'react-native';
+
+export default function Test(props) {
+    return (
+        <View>
+            <Text>Hello i am Testing</Text>
+        </View>    
+    );
+}
