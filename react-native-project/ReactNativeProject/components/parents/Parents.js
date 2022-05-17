b6598363390e2b5557d3dd7c63e64ab591f155e9
Azam <azam3cs@gmail.com>
2022-05-17 22:59:23 +0200

    added icons for bottomTabBar

--- react-native-project/ReactNativeProject/components/parents/Parents.js
+++ react-native-project/ReactNativeProject/components/parents/Parents.js
@@ -5,6 +5,7 @@ import {
     Text
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Test from './Test';
@@ -15,13 +16,24 @@ const Tab = createBottomTabNavigator();
 export default function Parents(props) {
     return (
         <Tab.Navigator
-            screenOptions={{
-                    activeTintColor: '#000',
-                    inactiveTintColor: 'gray',
+                screenOptions={({ route }) => ({
+                    tabBarActiveTintColor: '#000',
+                    tabBarInactiveTintColor: 'gray',
                     style: {
                         backgroundColor: '#F5F5F5',
                     },
-                }}
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        if (route.name == 'Uppdrag') {
+                            iconName = focused ? 'home' : 'home';
+                        } else if (route.name == 'Köpta') {
+                            iconName = focused ? 'shopping-bag' : 'shopping-bag';
+                        } else if (route.name == 'Vunna') {
+                            iconName = focused ? 'trophy' : 'trophy';
+                        } 
+                        return <Icon name={iconName} size={20} color={color} />;            
+                    },
+                })}
             >
             <Tab.Screen
                 name="Uppdrag"
