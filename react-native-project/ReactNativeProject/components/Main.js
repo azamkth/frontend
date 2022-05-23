02429febb67752dddff53439e149aac87dd68a97
Azam <azam3cs@gmail.com>
2022-05-23 08:37:13 +0200

    Update Main.js

--- react-native-project/ReactNativeProject/components/Main.js
+++ react-native-project/ReactNativeProject/components/Main.js
@@ -3,7 +3,7 @@ import {
     View,
     Text,
     TouchableOpacity,
-    SafeAreaView,
+    StyleSheet
 } from 'react-native';
  
 import { NavigationContainer } from '@react-navigation/native';
@@ -14,19 +14,19 @@ import ChildTabs from './childrens/ChildTabs';
 
 function HomeScreen(props) {
     return (
-        <View>
-            <SafeAreaView>
-                <TouchableOpacity 
-                    onPress={() => props.navigation.navigate('ParentTabs')}
-                >
-                    <Text style={{color:'#000'}}>Parents</Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                    onPress={() => props.navigation.navigate('ChildTabs')}
-                >
-                    <Text style={{color:'#000'}}>Children</Text>
-                </TouchableOpacity>
-            </SafeAreaView>
+        <View style={styles.container}>
+            <TouchableOpacity 
+                style={styles.primaryBtn}
+                onPress={() => props.navigation.navigate('ParentTabs')}
+            >
+                <Text style={styles.btnTxt}>Parent</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+                style={[styles.primaryBtn, {marginTop:20}]}
+                onPress={() => props.navigation.navigate('ChildTabs')}
+            >
+                <Text style={styles.btnTxt}>Children</Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -56,5 +56,28 @@ export default function Main() {
         </NavigationContainer>
     );
 };
+
+const white = '#ffffff';
+const darkBlue = '#262652';
+
+const styles = StyleSheet.create({
+    container: {
+       flex: 1,
+       alignItems:'center',
+       justifyContent:'center',
+       backgroundColor: darkBlue,
+    },
+    primaryBtn: {
+        width:120, 
+        padding:8, 
+        borderColor:white, 
+        borderWidth:1, 
+        borderRadius:8
+    },
+    btnTxt: {
+        color:'#fff', 
+        textAlign:'center'
+    }
+});
 
  
